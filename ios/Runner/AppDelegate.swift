eaed5d2e8c72d83cde98fffdc947f6d23e1c5e54
Ralph Kevin Rynard Macahipay <201980122@psu.palawan.edu.ph>
2022-08-31 09:37:02 +0800

    Step-3

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBb3DrlDx8BLhXvsUI6BxJb3i-Wdvlaigo")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/pages_thesis_app/home_page.dart
+++ lib/pages_thesis_app/home_page.dart
@@ -1,8 +1,14 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:async';
+
 import 'package:accounts/enum/enum.dart';
 import 'package:accounts/routes/route_pages.dart';
 import 'package:accounts/services/auth/auth_service.dart';
 import 'package:accounts/utility/logout_dialog.dart';
 import 'package:flutter/material.dart';
+
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,6 +18,24 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final Completer<GoogleMapController> _controller = Completer();
+  // ignore: prefer_const_constructors
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final Marker _kGooglePlexMarker = Marker(
+    markerId: MarkerId("_kGooglePlex"),
+    infoWindow: InfoWindow(title: "Google Plex"),
+    icon: BitmapDescriptor.defaultMarker,
+    position: LatLng(37.42796133580664, -122.085749655962),
+  );
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
   @override
   Widget build(BuildContext context) {
     //-----------------------------------logout button-------------------
@@ -37,12 +61,24 @@ class _HomePageState extends State<HomePage> {
       ];
     });
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('This is homepage'),
-        actions: [
-          threeDOtsButton,
-        ],
+      body: GoogleMap(
+        mapType: MapType.normal,
+        markers: {_kGooglePlexMarker},
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: const Text('To the lake!'),
+        icon: const Icon(Icons.directions_boat),
       ),
     );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
